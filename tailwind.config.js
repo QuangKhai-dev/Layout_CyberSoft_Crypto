31a9d7134143042563f7d164226afb80433bd58e
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-06-27 02:28:29 +0700

    update layout 70%

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: ["./**/*.html", "./**/*.js"],
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      backgroundImage: {
+        "pink-gradient":
+          "linear-gradient(90deg, #FF9DAB 1.51%, #FF4595 99.06%);",
+      },
+    },
+  },
+  plugins: [],
+};
