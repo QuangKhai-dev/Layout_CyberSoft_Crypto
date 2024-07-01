fef0e45b9a64dfa4b382ffb915a9b91dd71f28de
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-07-01 16:19:35 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         "pink-gradient":
           "linear-gradient(90deg, #FF9DAB 1.51%, #FF4595 99.06%);",
       },
+      flex: {
+        2: "2 2 0%",
+      },
     },
   },
   plugins: [],
