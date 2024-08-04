1884717b8c6afbdb39f8b0b6b2f26d95e2d73144
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-08-05 03:35:15 +0700

    update project mobile reponsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
     extend: {
+      // screens: {
+      //   sm: { max: "640px" }, // Tối đa 640px
+      //   md: { max: "768px" }, // Tối đa 768px
+      //   lg: { max: "1024px" }, // Tối đa 1024px
+      //   xl: { max: "1280px" }, // Tối đa 1280px
+      //   "2xl": { max: "1536px" }, // Tối đa 1536px
+      // },
       container: {
         center: true,
       },
